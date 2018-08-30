401fb1fa11b48593de95ae383623138af242ebfd
Slava Morza <bloodreek@gmail.com>
2018-08-30 14:09:54 +0300

    .

--- IBAstore/Models/StoreContext.cs
+++ IBAstore/Models/StoreContext.cs
@@ -30,24 +30,50 @@ namespace IBAstore.Models
     {
         protected override void Seed(StoreContext db)
         {
-            db.Roles.Add(new ApplicationRole { Name = "Admin" });
-            db.Roles.Add(new ApplicationRole { Name = "User" });
-            var userstore = new UserStore<ApplicationUser>(db);
-            var usermanager = new UserManager<ApplicationUser>(userstore);
-            //ApplicationUser admin = new ApplicationUser();
-            //admin.UserName = "admin";
-            //usermanager.Create(admin, "admin");
-            //usermanager.AddToRole(admin.Id, "Admin");
-            ApplicationUser user = new ApplicationUser { UserName = "admin" };
-            IdentityResult result = usermanager.Create(user, "admin");
+            //db.Roles.Add(new ApplicationRole { Name = "Admin" });
+            //db.Roles.Add(new ApplicationRole { Name = "User" });
+            //var userstore = new UserStore<ApplicationUser>(db);
+            //var usermanager = new UserManager<ApplicationUser>(userstore);
+            ////ApplicationUser admin = new ApplicationUser();
+            ////admin.UserName = "admin";
+            ////usermanager.Create(admin, "admin");
+            ////usermanager.AddToRole(admin.Id, "Admin");
+            //ApplicationUser user = new ApplicationUser { UserName = "admin" };
+            //IdentityResult result = usermanager.Create(user, "admin");
+            //if (result.Succeeded)
+            //{
+            //    usermanager.AddToRole(user.Id, "Admin");
+            //    Cart cart = new Cart
+            //    {
+            //        UserId = user.Id,
+            //    };
+            //    db.Carts.Add(cart);                
+            //}
+            var userManager = new ApplicationUserManager(new UserStore<ApplicationUser>(db));
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+
+            
+            var role1 = new IdentityRole { Name = "Admin" };
+            var role2 = new IdentityRole { Name = "User" };
+
+            
+            roleManager.Create(role1);
+            roleManager.Create(role2);
+
+            
+            var admin = new ApplicationUser { UserName = "admin" };
+            string password = "adminiba";
+            var result = userManager.Create(admin, password);
+            
             if (result.Succeeded)
-            {
-                usermanager.AddToRole(user.Id, "Admin");
+            {                
+                userManager.AddToRole(admin.Id, role1.Name);
+                userManager.AddToRole(admin.Id, role2.Name);
                 Cart cart = new Cart
                 {
-                    UserId = user.Id,
+                    UserId = admin.Id,
                 };
-                db.Carts.Add(cart);                
+                db.Carts.Add(cart);
             }
             base.Seed(db);
         }
